77b86e7fcd49eb3ee3f5e6e5ebf27d20fb656106
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-21 23:35:16 -0500

    add kittens route and correct actions into pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,29 @@ get 'welcome' => 'pages#welcome'
 get 'about' => 'pages#about'
 get 'contest' => 'pages#contest'
 get 'kitten/:size' => 'pages#kitten'
+get 'kittens/:size' => 'pages#kittens'
 
-def welcome
-  @header = "I don't know what page this is..."
-end
 
-def kitten
-  requested_size = params[:size]
-  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
-end
+
+
+
+#
+#
+# def kitten
+#   set_kitten_url
+# end
+#
+# def kittens
+#   set_kitten_url
+# end
+#
+#
+# def set_kitten_url
+#   requested_size = params[:size]
+#   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+# end
+
+
 
 
 
