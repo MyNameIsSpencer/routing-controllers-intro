92a8124c20abf825143d9f9427cdbf9be5b396cf
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-21 23:16:47 -0500

    hopefully get cat pic working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,19 @@ get '/' => 'pages#welcome'
 get 'welcome' => 'pages#welcome'
 get 'about' => 'pages#about'
 get 'contest' => 'pages#contest'
+get 'kitten/:size' => 'pages#kitten'
+
+def welcome
+  @header = "I don't know what page this is..."
+end
+
+def kitten
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
